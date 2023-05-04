5ff5c060036fe58b9709e25f76d6e494a547329c
Alex Zaloga <alexandrazaloga@gmail.com>
2023-05-03 20:35:55 -0400

    initial develop commit- making develop branch

---
+++ src/pages/Page/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import "./HomePage.scss";
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <p>hi</p>
+    </div>
+  );
+}
+export default HomePage;
---
+++ src/pages/Page/UploadPage/UploadPage.js
@@ -0,0 +1,10 @@
+import "./UploadPage.scss";
+
+function UploadPage() {
+  return (
+    <div className="UploadPage">
+      <p>hi</p>
+    </div>
+  );
+}
+export default UploadPage;
